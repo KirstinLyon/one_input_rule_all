997b903f7e365309ce340aa8fc2a8bb7e23208f6
kirstinlyon <kirstin@mltwelve.com>
2025-06-17 21:11:51 +0200

    documentation added

--- Scripts/fetch_data.R
+++ Scripts/fetch_data.R
@@ -12,89 +12,87 @@
 # CHANGES:      2025-06-17 - Initial version
 
 
-
+# Load required libraries----------------------------------------------------
 
 library(tidyverse)
 library(janitor)
 
- DATA <- "Data/Enhanced_Geographical_Analysis.txt"
- INDICATORS <- c("TX_NEW","HTS_TST","HTS_TST_POS", "TB_STAT", "TB_STAT_POS")
- EXCLUDE_FISCAL_YEAR <- c("2016", "2017", "2018")
 
+# Define constants and parameters--------------------------------------------
 
-
+DATA <- "Data/Enhanced_Geographical_Analysis.txt"
+INDICATORS <- c("TX_NEW","HTS_TST","HTS_TST_POS", "TB_STAT", "TB_STAT_POS")
+EXCLUDE_FISCAL_YEAR <- c("2016", "2017", "2018")
 SADC_COUNTRY <- c("Angola", 
-                                       "Botswana",
-                                      "Comoros", # not in dataset
-                                       "Democratic Republic of the Congo",
-                                       "Eswatini", 
-                                       "Lesotho", 
-                                       "Madagascar", #not in dataset
-                                       "Malawi", 
-                                       "Mauritius", #not in dataset
-                                       "Mozambique", 
-                                       "Namibia", 
-                                       "Seychelles", #not in dataset
-                                       "South Africa",
-                                       "Tanzania", 
-                                       "Zambia", 
-                                       "Zimbabwe")
-
-
-
-
-
-
-
-
-
-
+                  "Botswana",
+                  "Comoros", # not in dataset
+                  "Democratic Republic of the Congo",
+                  "Eswatini", 
+                  "Lesotho", 
+                  "Madagascar", #not in dataset
+                  "Malawi", 
+                  "Mauritius", #not in dataset
+                  "Mozambique", 
+                  "Namibia", 
+                  "Seychelles", #not in dataset
+                  "South Africa",
+                  "Tanzania", 
+                  "Zambia", 
+                  "Zimbabwe")
 
 EXCLUDE_FISCAL_YEAR <- c("2016", "2017", "2018", "2019")
 
+# Function to prepare data for analysis--------------------------------------
+#' Process PEPFAR data
+#'
+#' @param data Enhanced geographical dataset
+#'
+#' @returns clean dataset
+#' @export
+#'
+#' @examples
+#' prep_data(data)
 
- prep_data <- function(data){
-         temp <- data |> 
-                 select(-matches(EXCLUDE_FISCAL_YEAR)) |> 
-             pivot_longer(cols = matches("results$"), names_to = "period", values_to = "value") |> 
-                 separate(period, into = c("fiscal_year", "period_type", "quarter", "misc"), sep = "_", fill = "right") |> 
-                 select(-c(period_type, misc)) |> 
-         
-         
-                 mutate(fiscal_year = str_remove(fiscal_year, "^x"),
-                                         period = paste0(fiscal_year, " Q", quarter)
-                                         ) |> 
-                 select(-c(fiscal_year, quarter))
-     
-             
-             return(temp)
-     }
+prep_data <- function(data){
+    temp <- data |> 
+        select(-matches(EXCLUDE_FISCAL_YEAR)) |> 
+        pivot_longer(cols = matches("results$"), names_to = "period", values_to = "value") |> 
+        separate(period, into = c("fiscal_year", "period_type", "quarter", "misc"), sep = "_", fill = "right") |> 
+        select(-c(period_type, misc)) |> 
+        
+        
+        mutate(fiscal_year = str_remove(fiscal_year, "^x"),
+               period = paste0(fiscal_year, " Q", quarter)
+        ) |> 
+        select(-c(fiscal_year, quarter))
+    
+    
+    return(temp)
+}
 
 
 
 # LOAD AND CLEAN DATA ---------------------------------------------------------
- my_data <- read_tsv(DATA) |> 
-         clean_names() |> 
-         filter(indicator %in% INDICATORS,
-                             country %in% SADC_COUNTRY
-                             ) |> 
-         select (-c(operating_unit, iso3,
-                                     sub_national_unit_3_uid,
-                                     sub_national_unit_2_uid,
-                                     sub_national_unit_1_uid,
-                                     sub_national_unit_3,
-                                     sub_national_unit_2,
-                                     sub_national_unit_1,
-                                     modality
-                                     )) |> 
-        mutate(indicator = case_when(indicator == "TB_STAT" ~ paste(indicator, numerator_denominator, sep = "_"),
-                                                                             .default = indicator)
-                             ) |> 
-         select(-numerator_denominator) |> 
-         group_by(country, indicator, description) |>
-         summarise(across(matches("Results$"), ~sum(.x, na.rm = TRUE)), .groups = "drop") |> 
-         prep_data() 
+my_data <- read_tsv(DATA) |> 
+    clean_names() |> 
+    filter(indicator %in% INDICATORS,
+           country %in% SADC_COUNTRY
+    ) |> 
+    select (-c(operating_unit, iso3,
+               sub_national_unit_3_uid,
+               sub_national_unit_2_uid,
+               sub_national_unit_1_uid,
+               sub_national_unit_3,
+               sub_national_unit_2,
+               sub_national_unit_1,
+               modality
+    )) |> 
+    mutate(indicator = case_when(indicator == "TB_STAT" ~ paste(indicator, numerator_denominator, sep = "_"),
+                                 .default = indicator)
+    ) |> 
+    select(-numerator_denominator) |> 
+    group_by(country, indicator, description) |>
+    summarise(across(matches("Results$"), ~sum(.x, na.rm = TRUE)), .groups = "drop") |> 
+    prep_data() 
 
-
-
- write_csv(my_data, "Dataout/clean_pepfar_test_data.csv")
+write_csv(my_data, "Dataout/clean_pepfar_test_data.csv")
